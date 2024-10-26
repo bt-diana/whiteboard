25ce6d6313656fefd3b2fd0eeba5f6f15275ab41
Diana Butiakova <butyakova01@mail.ru>
2024-10-27 00:03:35 +0600

    feat: able to drop draggable only on droppable

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -10,7 +10,6 @@ if (droppable && draggables) {
         draggable.addEventListener('mousedown', handleMouseDown);
 
         function handleMouseDown(e: MouseEvent): void {
-            console.log(e.relatedTarget)
             x = e.clientX;
             y = e.clientY;
         
@@ -18,8 +17,7 @@ if (droppable && draggables) {
             draggable.addEventListener('mouseup', handleMouseUp);
         }
         
-        function handleMouseMove(e: MouseEvent): void {
-            console.log('mousemove');
+        function handleMouseMove(e: MouseEvent): void {\
             draggable.style.left = `${draggable.offsetLeft - x + e.clientX}px`
             draggable.style.top = `${draggable.offsetTop - y + e.clientY}px`
         
@@ -28,6 +26,18 @@ if (droppable && draggables) {
         }
         
         function handleMouseUp(e: MouseEvent): void {
+            if (draggable.offsetLeft < droppable.offsetLeft) {
+                draggable.style.left = `${droppable.offsetLeft}px`;
+            } else if (draggable.offsetLeft + draggable.offsetWidth > droppable.offsetLeft + droppable.offsetWidth) {
+                draggable.style.left = `${droppable.offsetLeft + droppable.offsetWidth - draggable.offsetWidth}px`;
+            }
+
+            if (draggable.offsetTop < droppable.offsetTop) {
+                draggable.style.top = `${droppable.offsetTop}px`;
+            } else if (draggable.offsetTop + draggable.offsetHeight > droppable.offsetTop + droppable.offsetHeight) {
+                draggable.style.top = `${droppable.offsetTop + droppable.offsetHeight - draggable.offsetHeight}px`;
+            }
+
             droppable.removeEventListener('mousemove', handleMouseMove);
             draggable.removeEventListener('mouseup', handleMouseUp);
         }
