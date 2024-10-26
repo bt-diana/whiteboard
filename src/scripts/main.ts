30be30503ef0952c96b92ffb43cd96e7958bd2ce
Diana Butiakova <butyakova01@mail.ru>
2024-10-26 23:36:59 +0600

    refactor: simpyfy code structure

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,46 +1,35 @@
 import '../styles/main.sass';
 
-type DragAndDropElement = {
-    draggable: HTMLElement;
-    droppable: HTMLElement;
-    x: number;
-    y: number;
-}
-
 const droppable: HTMLElement | null = document.querySelector<HTMLElement>('.droppable') ? document.querySelector<HTMLElement>('.droppable') : document.body;
 const draggables: Array<HTMLElement> | null = document.querySelectorAll<HTMLElement>('.draggable') ? [...document.querySelectorAll<HTMLElement>('.draggable')] : null;
 
 if (droppable && draggables) {
     draggables.forEach((draggable) => {
-        const dragAndDropElement: DragAndDropElement = {
-            draggable: draggable,
-            droppable: droppable,
-            x: 0,
-            y: 0
-        }
+        let x: number, y: number;
 
-        dragAndDropElement.draggable.addEventListener('mousedown', handleMouseDown);
+        draggable.addEventListener('mousedown', handleMouseDown);
 
         function handleMouseDown(e: MouseEvent): void {
-            dragAndDropElement.x = e.clientX;
-            dragAndDropElement.y = e.clientY;
+            console.log(e.relatedTarget)
+            x = e.clientX;
+            y = e.clientY;
         
-            dragAndDropElement.droppable.addEventListener('mousemove', handleMouseMove);
-            dragAndDropElement.draggable.addEventListener('mouseup', handleMouseUp);
+            droppable.addEventListener('mousemove', handleMouseMove);
+            draggable.addEventListener('mouseup', handleMouseUp);
         }
         
         function handleMouseMove(e: MouseEvent): void {
             console.log('mousemove');
-            dragAndDropElement.draggable.style.left = `${dragAndDropElement.draggable.offsetLeft - dragAndDropElement.x + e.clientX}px`
-            dragAndDropElement.draggable.style.top = `${dragAndDropElement.draggable.offsetTop - dragAndDropElement.y + e.clientY}px`
+            draggable.style.left = `${draggable.offsetLeft - x + e.clientX}px`
+            draggable.style.top = `${draggable.offsetTop - y + e.clientY}px`
         
-            dragAndDropElement.x = e.clientX;
-            dragAndDropElement.y = e.clientY;
+            x = e.clientX;
+            y = e.clientY;
         }
         
         function handleMouseUp(e: MouseEvent): void {
-            dragAndDropElement.droppable.removeEventListener('mousemove', handleMouseMove);
-            dragAndDropElement.draggable.removeEventListener('mouseup', handleMouseUp);
+            droppable.removeEventListener('mousemove', handleMouseMove);
+            draggable.removeEventListener('mouseup', handleMouseUp);
         }
     });    
 }
