15b6d205075ae9e38ab4aab5dac94e0eb8db336a
Diana Butiakova <butyakova01@mail.ru>
2024-10-26 19:18:20 +0600

    feat: able to move more than one draggables independently

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,42 +1,46 @@
 import '../styles/main.sass';
 
-type Draggable = {
-    element: HTMLElement;
+type DragAndDropElement = {
+    draggable: HTMLElement;
+    droppable: HTMLElement;
     x: number;
     y: number;
 }
 
-const draggableElement: HTMLElement | null = document.querySelector('.draggable');
-if (!draggableElement) {
-    throw Error('No draggable element found');
-}
+const droppable: HTMLElement | null = document.querySelector<HTMLElement>('.droppable') ? document.querySelector<HTMLElement>('.droppable') : document.body;
+const draggables: Array<HTMLElement> | null = document.querySelectorAll<HTMLElement>('.draggable') ? [...document.querySelectorAll<HTMLElement>('.draggable')] : null;
 
-const draggable: Draggable = {
-    element: draggableElement,
-    x: 0,
-    y: 0
-}
+if (droppable && draggables) {
+    draggables.forEach((draggable) => {
+        const dragAndDropElement: DragAndDropElement = {
+            draggable: draggable,
+            droppable: droppable,
+            x: 0,
+            y: 0
+        }
 
-draggable.element.addEventListener('mousedown', handleMouseDown);
+        dragAndDropElement.draggable.addEventListener('mousedown', handleMouseDown);
 
-function handleMouseDown(e: MouseEvent): void {
-    draggable.x = e.clientX;
-    draggable.y = e.clientY;
-
-    draggable.element.addEventListener('mousemove', handleMouseMove);
-    draggable.element.addEventListener('mouseup', handleMouseUp);
-    draggable.element.addEventListener('mouseout', handleMouseUp);
-}
-
-function handleMouseMove(e: MouseEvent): void {
-    draggable.element.style.left = `${draggable.element.offsetLeft - draggable.x + e.clientX}px`
-    draggable.element.style.top = `${draggable.element.offsetTop - draggable.y + e.clientY}px`
-
-    draggable.x = e.clientX;
-    draggable.y = e.clientY;
-}
-
-function handleMouseUp(e: MouseEvent): void {
-    draggable.element.removeEventListener('mousemove', handleMouseMove);
-    draggable.element.removeEventListener('mouseup', handleMouseUp);
+        function handleMouseDown(e: MouseEvent): void {
+            dragAndDropElement.x = e.clientX;
+            dragAndDropElement.y = e.clientY;
+        
+            dragAndDropElement.droppable.addEventListener('mousemove', handleMouseMove);
+            dragAndDropElement.draggable.addEventListener('mouseup', handleMouseUp);
+        }
+        
+        function handleMouseMove(e: MouseEvent): void {
+            console.log('mousemove');
+            dragAndDropElement.draggable.style.left = `${dragAndDropElement.draggable.offsetLeft - dragAndDropElement.x + e.clientX}px`
+            dragAndDropElement.draggable.style.top = `${dragAndDropElement.draggable.offsetTop - dragAndDropElement.y + e.clientY}px`
+        
+            dragAndDropElement.x = e.clientX;
+            dragAndDropElement.y = e.clientY;
+        }
+        
+        function handleMouseUp(e: MouseEvent): void {
+            dragAndDropElement.droppable.removeEventListener('mousemove', handleMouseMove);
+            dragAndDropElement.draggable.removeEventListener('mouseup', handleMouseUp);
+        }
+    });    
 }
