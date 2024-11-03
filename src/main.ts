c6a334c6b43e9b3c2b1f780dc8655fb4bd50eddf
Diana Butiakova <butyakova01@gmail.com>
2024-11-03 23:49:43 +0600

    feat: able to add multiple emojis from menu

--- src/main.ts
+++ src/main.ts
@@ -2,51 +2,64 @@ import './main.sass';
 import emojis from './emoji';
 
 
-const droppable: HTMLElement | null = document.querySelector<HTMLElement>('.droppable') ? document.querySelector<HTMLElement>('.droppable') : document.body;
-// const draggablesContainer: HTMLElement | null = document.querySelector<HTMLElement>('.draggables-container');
+const droppable: HTMLElement | null = document.querySelector<HTMLElement>('.droppable');
+const menu: HTMLElement | null = document.querySelector<HTMLElement>('.menu');
 
-if (droppable && emojis) {
+if (droppable && menu && emojis) {
     emojis.forEach((emoji) => {
         let x: number, y: number;
-        const draggable: HTMLElement = document.createElement('div');
-        draggable.classList.add('draggable', 'emoji');
-        draggable.innerHTML = emoji;
+        const menuItem: HTMLElement = document.createElement('div');
+        menuItem.classList.add('menu__item', 'emoji');
+        menuItem.innerHTML = emoji;
 
-        droppable.appendChild(draggable);    
+        menu.appendChild(menuItem);    
         
-        draggable.addEventListener('mousedown', handleMouseDown);
+        menuItem.addEventListener('mousedown', handleDragMenuItem);
 
-        function handleMouseDown(e: MouseEvent): void {
-            x = e.clientX;
-            y = e.clientY;
-        
-            droppable.addEventListener('mousemove', handleMouseMove);
-            draggable.addEventListener('mouseup', handleMouseUp);
-        }
-        
-        function handleMouseMove(e: MouseEvent): void {
-            draggable.style.left = `${draggable.offsetLeft - x + e.clientX}px`
-            draggable.style.top = `${draggable.offsetTop - y + e.clientY}px`
-        
-            x = e.clientX;
-            y = e.clientY;
-        }
-        
-        function handleMouseUp(e: MouseEvent): void {
-            if (draggable.offsetLeft < droppable.offsetLeft) {
-                draggable.style.left = `${droppable.offsetLeft}px`;
-            } else if (draggable.offsetLeft + draggable.offsetWidth > droppable.offsetLeft + droppable.offsetWidth) {
-                draggable.style.left = `${droppable.offsetLeft + droppable.offsetWidth - draggable.offsetWidth}px`;
+        function handleDragMenuItem(e: MouseEvent): void {
+            const draggable: HTMLElement = document.createElement('div');
+            draggable.classList.add('draggable', 'emoji');
+            draggable.innerHTML = emoji;
+            
+            const emojiImg: HTMLElement | null = draggable.querySelector<HTMLElement>('.emoji img');
+            emojiImg?.setAttribute('draggable', 'false')
+
+            droppable.appendChild(draggable);
+
+            draggable.addEventListener('mousedown', handleMouseDown);
+
+            function handleMouseDown(e: MouseEvent): void {
+                x = e.clientX;
+                y = e.clientY;
+            
+                droppable.addEventListener('mousemove', handleMouseMove);
+                draggable.addEventListener('mouseup', handleMouseUp);
             }
-
-            if (draggable.offsetTop < droppable.offsetTop) {
-                draggable.style.top = `${droppable.offsetTop}px`;
-            } else if (draggable.offsetTop + draggable.offsetHeight > droppable.offsetTop + droppable.offsetHeight) {
-                draggable.style.top = `${droppable.offsetTop + droppable.offsetHeight - draggable.offsetHeight}px`;
+            
+            function handleMouseMove(e: MouseEvent): void {
+                draggable.style.left = `${draggable.offsetLeft - x + e.clientX}px`
+                draggable.style.top = `${draggable.offsetTop - y + e.clientY}px`
+            
+                x = e.clientX;
+                y = e.clientY;
             }
-
-            droppable.removeEventListener('mousemove', handleMouseMove);
-            draggable.removeEventListener('mouseup', handleMouseUp);
+            
+            function handleMouseUp(e: MouseEvent): void {
+                if (draggable.offsetLeft < droppable.offsetLeft) {
+                    draggable.style.left = `${droppable.offsetLeft}px`;
+                } else if (draggable.offsetLeft + draggable.offsetWidth > droppable.offsetLeft + droppable.offsetWidth) {
+                    draggable.style.left = `${droppable.offsetLeft + droppable.offsetWidth - draggable.offsetWidth}px`;
+                }
+    
+                if (draggable.offsetTop < droppable.offsetTop) {
+                    draggable.style.top = `${droppable.offsetTop}px`;
+                } else if (draggable.offsetTop + draggable.offsetHeight > droppable.offsetTop + droppable.offsetHeight) {
+                    draggable.style.top = `${droppable.offsetTop + droppable.offsetHeight - draggable.offsetHeight}px`;
+                }
+    
+                droppable.removeEventListener('mousemove', handleMouseMove);
+                draggable.removeEventListener('mouseup', handleMouseUp);
+            }
         }
     });    
 }
